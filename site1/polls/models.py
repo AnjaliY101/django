e4adf248cfe52a36a638785d6ab3b5ebfc96ec41
Reddirteater <137114167+Reddirteater@users.noreply.github.com>
2024-01-09 21:29:35 +0000

    Updated models(mini classes)

--- site1/polls/models.py
+++ site1/polls/models.py
@@ -2,11 +2,17 @@ from django.db import models
 
 class Video(models.Model):
     Url =  models.CharField()
-    User = models.ForeignKey
-    "User"
+    User = models.ForeignKey("User")
+    Title = models.CharField()
+    Created = models.DateTimeField()
     on_delete = models.CASCADE
 # Create your models here.
 
 class User(models.Model):
     Username = models.CharField()
     Password = models.CharField()
+
+class Reactions(models.Model):
+    User = models.ForeignKey
+    Video = models.ForeignKey
+    Type = models.CharField()
